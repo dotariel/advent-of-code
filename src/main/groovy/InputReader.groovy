efa00181125f9bc28e29f8c1cb351e77d396b55a
Ariel Alonso <xsaint32@gmail.com>
2016-12-07 17:08:23 -0500

    Add InputReader helper class

---
+++ src/main/groovy/InputReader.groovy
@@ -0,0 +1,9 @@
+package aoc
+
+class InputReader {
+  public static String readInput(String resourcePath) {
+    def input = new InputReader().class.classLoader.getResource("${resourcePath}")
+
+    return new File(input.path).text
+  }
+}
