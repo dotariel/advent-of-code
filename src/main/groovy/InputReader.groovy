0117307be81239c9a6097f4021e5b4f898bbdffc
Ariel Alonso <xsaint32@gmail.com>
2016-12-07 17:09:30 -0500

    Refactor InputReader.readInput() method

--- src/main/groovy/InputReader.groovy
+++ src/main/groovy/InputReader.groovy
@@ -2,7 +2,7 @@ package aoc
 
 class InputReader {
   public static String readInput(String resourcePath) {
-    def input = new InputReader().class.classLoader.getResource("${resourcePath}")
+    def input = new InputReader().class.classLoader.getResource(resourcePath)
 
     return new File(input.path).text
   }
