da53a97eeba18b808fdf13db995010f74f60072b
Ariel Alonso <xsaint32@gmail.com>
2016-12-09 20:48:17 -0500

    Refactor Day6 test

--- test/unit/groovy/Day6/SignalTest.groovy
+++ test/unit/groovy/Day6/SignalTest.groovy
@@ -17,30 +17,15 @@ class SignalTest {
 
   @Test
   public void should_decode_with_max_strategy() {
-    def input = new StringBuilder()
-
-    input.append('eedadn' + '\n')
-    input.append('drvtee' + '\n')
-    input.append('eandsr' + '\n')
-    input.append('raavrd' + '\n')
-    input.append('atevrs' + '\n')
-    input.append('tsrnev' + '\n')
-    input.append('sdttsa' + '\n')
-    input.append('rasrtv' + '\n')
-    input.append('nssdts' + '\n')
-    input.append('ntnada' + '\n')
-    input.append('svetve' + '\n')
-    input.append('tesnvt' + '\n')
-    input.append('vntsnd' + '\n')
-    input.append('vrdear' + '\n')
-    input.append('dvrsen' + '\n')
-    input.append('enarar')
-
-    assert 'easter' == new Signal(input.toString()).decode()
+    assert 'easter' == new Signal(getInput()).decode()
   }
 
   @Test
   public void should_decode_with_min_strategy() {
+    assert 'advent' == new Signal(getInput(), new MinDecodingStrategy()).decode()
+  }  
+
+  private String getInput() {
     def input = new StringBuilder()
 
     input.append('eedadn' + '\n')
@@ -60,6 +45,6 @@ class SignalTest {
     input.append('dvrsen' + '\n')
     input.append('enarar')
 
-    assert 'advent' == new Signal(input.toString(), new MinDecodingStrategy()).decode()
-  }  
+    return input.toString()
+  }
 }
